438eb7a48b23b9ab8ac7a4290b6a9537d798b85d
Minjie Xu <Minjie.Xu@gmail.com>
2019-10-12 20:56:43 -0500

    add other (the remaining) files

---
+++ src/main/java/com/okta/developer/websockets/JavaWebSocketsApplication.java
@@ -0,0 +1,13 @@
+package com.okta.developer.websockets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebSocketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWebSocketsApplication.class, args);
+	}
+
+}
